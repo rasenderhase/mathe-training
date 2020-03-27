a124a01f436cc229bfcda7689cf3a70c8b0b8381
Andreas Knees <andreas.knees@nikem.de>
2020-03-27 03:41:41 +0100

    satisfy Safari

--- modules/calculation.js
+++ modules/calculation.js
@@ -1,2 +1,2 @@
-export * as SimpleMultiplication from "./calculation/simple-multiplication.js"
+export * from "./calculation/simple-multiplication.js"
 export { Task } from "./calculation/task.js"
--- modules/calculation/simple-multiplication.js
+++ modules/calculation/simple-multiplication.js
@@ -1,26 +1,28 @@
 import {Task} from "./task.js"
 import * as Utility from "../utility.js"
 
-export function createTask(paremeters) {
-    let mult1 = Utility.random(10);
-    let mult2 = Utility.random(10);
+export let SimpleMultiplication = {
+    createTask : function createTask(paremeters) {
+        let mult1 = Utility.random(10);
+        let mult2 = Utility.random(10);
 
-    let text = mult1 + "&middot;" + mult2 + " =&nbsp;";
+        let text = mult1 + "&middot;" + mult2 + " =&nbsp;";
 
-    let result = mult1 * mult2;
+        let result = mult1 * mult2;
 
-    let multipleChoiceResults = [];
-    if (paremeters.difficulty < 10) {
-        for (let i = 0; i < paremeters.difficulty; i++) {
-            let wrongResult;
+        let multipleChoiceResults = [];
+        if (paremeters.difficulty < 10) {
+            for (let i = 0; i < paremeters.difficulty; i++) {
+                let wrongResult;
 
-            do {
-                wrongResult = Utility.random(100);
-            } while (wrongResult === result || multipleChoiceResults.includes(wrongResult));
-            multipleChoiceResults.push(wrongResult);
+                do {
+                    wrongResult = Utility.random(100);
+                } while (wrongResult === result || multipleChoiceResults.includes(wrongResult));
+                multipleChoiceResults.push(wrongResult);
+            }
+            multipleChoiceResults.splice(Utility.random(0, paremeters.difficulty), 0, result);
         }
-        multipleChoiceResults.splice(Utility.random(0, paremeters.difficulty), 0, result);
-    }
 
-    return new Task(text, result, multipleChoiceResults);
+        return new Task(text, result, multipleChoiceResults);
+    }
 }
