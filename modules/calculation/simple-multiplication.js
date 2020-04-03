f8c05241b28a792bc03ba7e6fbb776efc2d665e7
Andreas Knees <andreas.knees@nikem.de>
2020-04-03 12:43:07 +0200

    1 mal1 einstellbar

--- modules/calculation/simple-multiplication.js
+++ modules/calculation/simple-multiplication.js
@@ -1,18 +1,18 @@
 import * as Task from "./task.js"
 import * as Utility from "../utility.js"
 
-const BASE_DIFFICULTY = 2;
+const BASE_DIFFICULTY = 1;
 
 export let SimpleMultiplication = {
-    createTask : function createTask(paremeters) {
-        let mult1 = Utility.random(10);
-        let mult2 = Utility.random(10);
+    createTask : function createTask(parameters) {
+        let mult1 = Utility.random(2, 10);
+        let mult2 = Utility.random(2, parameters.max);
 
         let text = mult1 + "&middot;" + mult2 + " =";
 
         let result = mult1 * mult2;
 
-        let max = 100;
-        return Task.createTask(paremeters, max, result, text, BASE_DIFFICULTY);
+        let max = parameters.max * 10;
+        return Task.createTask(parameters, max, result, text, BASE_DIFFICULTY + parameters.max / 10);
     }
 };
